97a639476f29c03c62b8b794d462c1db467c38c9
lizgarcia <lizgarcia@microsoft.com>
2022-04-17 08:16:41 -0700

    commit on 'main'

--- Functions/CreateRating/__init__.py
+++ Functions/CreateRating/__init__.py
@@ -6,17 +6,17 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('productId')
+    name = req.params.get('name')
     if not name:
         try:
-            req_body = req.get_json('name')
+            req_body = req.get_json()
         except ValueError:
             pass
         else:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello Billy, {name}. This HTTP triggered function executed successfully, awesome Billy.")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
